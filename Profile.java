8ec6430e964dc3419122e0564961ec0461925822
willcheesman <will.cheesman@outlook.com>
2026-03-31 14:24:36 +0100

    Add files via upload

---
+++ Profile.java
@@ -0,0 +1,29 @@
+package messageappgp;
+
+public class Profile {
+	private String Username;
+	private String Id;
+	private String Phonenum;
+
+public Profile(String Username, String Id, String Phonenum) {
+	this.Username = Username;
+	this.Id = Id;
+	this.Phonenum = Phonenum;
+}
+
+
+
+public String getUsername(){
+	return Username;
+}
+
+public String getId(){
+	return Id;
+}
+
+public String getPhonenum(){
+	return Phonenum;
+}
+}
+
+
