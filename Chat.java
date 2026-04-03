27d51b004fce0ad2c67bd951fc9b1d4c07d0a2e5
Callum Drumm <callumdrumm6@gmail.com>
2026-04-03 15:43:20 +0100

    getting back old chat class

--- Chat.java
+++ Chat.java
@@ -1,32 +1,86 @@
-package DataMessagingAppLanding;
+import javax.swing.*;
+import java.awt.event.*;
+import java.util.Scanner;
+import java.util.UUID;
+
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Set;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Deque;
+import java.util.HashSet;
+
 
 public class Chat {
+	JFrame frame1 = new JFrame();
+	
+	JButton send = new JButton("send");
+	
+	JTextField chatbox = new JTextField();
+	
+	JTextArea chatWindow = new JTextArea();
+	
+	Deque<Msg> queue = new LinkedList<>();
+	public Chat() {
+	send.setBounds(350, 600, 100, 50);
+	
+	
+	chatbox.setBounds(50,600, 300, 50);
+	
+	chatWindow.setBounds(50,100,400,500);
+	
+	
+	// add action listener
+		send.addActionListener(new ActionListener()
+		        {  
+		            public void actionPerformed(ActionEvent e)
+		            {  
+		            	String message = chatbox.getText();
+		                if (!message.isEmpty()) {
+		                	
+		                	Msg newMessage = new Msg();
+		                    
+		                    
+		                	newMessage.setCont(message);
+		                	newMessage.setTime(LocalTime.now());
+		                	queue.add(newMessage);
+		                    chatWindow.append("You: " + queue.getLast() + "\n");
+		                    chatbox.setText("");
+		            }  
+		            }
+		        });
+		        
+	
+	
+	frame1.add(send);
+	
+	frame1.add(chatbox);
+	
+	frame1.add(chatWindow);
+	
+	
+	frame1.setLayout(null);
+	
+	
+	frame1.setVisible(true);
+	
+	frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+	
+	
+	
+	
+	
 
-    private int id;
-    private String senderName;
-    private String readStatus;
-    private String time;
 
-    public Chat(int id, String senderName, String readStatus, String time) {
-        this.id = id;
-        this.senderName = senderName;
-        this.readStatus = readStatus;
-        this.time = time;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getSenderName() {
-        return senderName;
-    }
-
-    public String getReadStatus() {
-        return readStatus;
-    }
-
-    public String getTime() {
-        return time;
-    }
 }
+	
+}
+	
+
+	
+
+
+	
+	
