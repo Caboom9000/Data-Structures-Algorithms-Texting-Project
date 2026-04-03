969de31088f5ca6f0475183e5772e21e023f713c
willcheesman <will.cheesman@outlook.com>
2026-04-04 00:16:05 +0100

    it making me commit again idk why

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 
-<<<<<<< HEAD
+
 
 public class main {
     public static void main(String[] args) {
@@ -12,15 +12,3 @@ contactlist.addContact(new Contact("Fred", "3", "33333333333"));
 new LandingPageGUI(contactlist);
     }
 }
-=======
-public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-	        new LandingPageGUI();
-	    
-	}
-
-}
->>>>>>> 9d04b06e5a3721f11dc0a25a7daae812f0bd0ec3
